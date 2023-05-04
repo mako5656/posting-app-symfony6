fd22ce7590dec3afb5a829a900f6bb34d3982e95
MakotoIkami <makoto.ikami@gmail.com>
2023-05-04 11:58:10 +0900

    Twigの制御構造を使う

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -12,13 +12,13 @@ class HelloController extends AbstractController
       "Hello", "Hi", "Bye!"
     ];
 
-    #[Route('/{limit<\d+>?3}', name: 'app_index')]
+    #[Route('/{limit<\d+>?0}', name: 'app_index')]
     public function index(int $limit): Response
     {
         return $this->render(
             'hello/index.html.twig',
             [
-                'message' => implode(',', array_slice($this->messages, 0, $limit))
+                'messages' => array_slice($this->messages, 0, $limit)
             ]
         );
     }
