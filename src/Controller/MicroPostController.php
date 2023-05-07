6229688818b4494d4b814b42fdd4da07716860cf
MakotoIkami <makoto.ikami@gmail.com>
2023-05-07 11:37:10 +0900

    Controller側で投稿内容のデータの更新や削除を行う

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\MicroPost;
+use App\Repository\MicroPostRepository;
+use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +12,17 @@ use Symfony\Component\Routing\Annotation\Route;
 class MicroPostController extends AbstractController
 {
     #[Route('/micro-post', name: 'app_micro_post')]
-    public function index(): Response
+    public function index(MicroPostRepository $posts): Response
     {
+//        $microPost = new MicroPost();
+//        $microPost->setTitle('It comes from controller');
+//        $microPost->setText('HI!');
+//        $microPost->setCreated(new DateTime());
+
+        $microPost = $posts->find(4);
+        $microPost->setTitle('Welcome in general!');
+        $posts->save($microPost, true);
+
         return $this->render('micro_post/index.html.twig', [
             'controller_name' => 'MicroPostController',
         ]);
