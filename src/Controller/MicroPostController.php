9d36f7b56532457a22ea8bcb36d691d3d9019525
MakotoIkami <makoto.ikami@gmail.com>
2023-05-13 16:40:48 +0900

    feat: 投稿の編集機能を実装

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -58,4 +58,32 @@ class MicroPostController extends AbstractController
             ]
         );
     }
+
+    #[Route('/micro-post/{post}/edit', name: 'app_micro_post_edit')]
+    public function edit(MicroPost $post, Request $request, MicroPostRepository $posts): Response
+    {
+        $form = $this->createFormBuilder($post)
+            ->add('title')
+            ->add('text')
+            ->add('submit', SubmitType::class, ['label' => 'Save'])
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $post = $form->getData();
+            $posts->save($post, true);
+
+            $this->addFlash('success', 'Your micro post have been updated');
+
+            return $this->redirectToRoute('app_micro_post');
+        }
+
+        return $this->renderForm(
+            'micro_post/add.html.twig',
+            [
+                'form' => $form
+            ]
+        );
+    }
 }
