cf89d83112c281bd2d66c41430b2815b718951b2
MakotoIkami <makoto.ikami@gmail.com>
2023-07-08 21:21:06 +0900

    MicroPostとCommentのEntityを作成

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -16,6 +16,10 @@ class Comment
     #[ORM\Column(length: 500)]
     private ?string $text = null;
 
+    #[ORM\ManyToOne(inversedBy: 'comments')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?MicroPost $post = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -29,6 +33,18 @@ class Comment
     public function setText(string $text): self
     {
         $this->text = $text;
+
+        return $this;
+    }
+
+    public function getPost(): ?MicroPost
+    {
+        return $this->post;
+    }
+
+    public function setPost(?MicroPost $post): self
+    {
+        $this->post = $post;
 
         return $this;
     }
