9572c6153267267ebc88746042b280f23fc40971
MakotoIkami <makoto.ikami@gmail.com>
2023-07-10 01:01:30 +0900

    OneToManyの関係を確認

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
+use App\Entity\MicroPost;
 use App\Entity\User;
 use App\Entity\UserProfile;
+use App\Repository\CommentRepository;
+use App\Repository\MicroPostRepository;
 use App\Repository\UserProfileRepository;
+use DateTime;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -18,8 +23,20 @@ class HelloController extends AbstractController
     ];
 
     #[Route('/', name: 'app_index')]
-    public function index(UserProfileRepository $profiles): Response
+    public function index(MicroPostRepository $posts, CommentRepository $comments): Response
     {
+//        $post = new MicroPost();
+//        $post->setTitle('Hello');
+//        $post->setText('Hello');
+//        $post->setCreated(new DateTime());
+
+//        $post = $posts->find(5);
+//        $comment = $post->getComments()[0];
+//        $comment->setPost(null);
+//        $comments->add($comment, true);
+
+//        dd($post);
+
 //        $user = new User();
 //        $user->setEmail('email@email.com');
 //        $user->setPassword('12345');
